a600b205a9f22aa7bbebbe2fa6f4afaeaf50e107
snakewizardd <83378208+snakewizardd@users.noreply.github.com>
2024-09-29 17:47:13 -0400

    iterative workflow generator

--- flowiseWorkflow.R
+++ flowiseWorkflow.R
@@ -181,30 +181,173 @@ performFirstRun(pnull='Successfuly conduct the Design-Build-Team for generating 
                             'Its been a difficult restructuring',
                             'I personally have moved from a 3 person team to 45 people', sep = ' '))
 
-iterationRow[1,'pnull']
-iterationRow[1,'I1']
-iterationRow[1,'D']
-iterationRow[1,'Fn']
-iterationRow[1,'Idelta']
-iterationRow[1,'TIdelta']
-iterationRow[1,'newP']
+# iterationRow[1,'pnull']
+# iterationRow[1,'I1']
+# iterationRow[1,'D']
+# iterationRow[1,'Fn']
+# iterationRow[1,'Idelta']
+# iterationRow[1,'TIdelta']
+# iterationRow[1,'newP']
 
-InfoRow <- rbind(data.frame(Row = paste0('Old Goal: ',iterationRow[1,'pnull'])),
-data.frame(Row = paste0('Original Information: ',iterationRow[1,'I1'])),
-data.frame(Row = paste0('Choices Considered: ',iterationRow[1,'D'])),
-data.frame(Row = paste0('Choice Chosen: ',iterationRow[1,'Fn'])),
-data.frame(Row = paste0('New Information From Run: ',iterationRow[1,'Idelta'])))
+InfoRow <- rbind(data.frame(Type= 'Original Goal',Row = paste0('Old Goal: ',iterationRow[1,'pnull'])),
+data.frame(Type= 'Original Information',Row = paste0('Original Information: ',iterationRow[1,'I1'])),
+data.frame(Type= 'Choices Considered',Row = paste0('Choices Considered: ',iterationRow[1,'D'])),
+data.frame(Type= 'Choice Chosen',Row = paste0('Choice Chosen: ',iterationRow[1,'Fn'])),
+data.frame(Type= 'New Information From Run',Row = paste0('New Information From Run: ',iterationRow[1,'Idelta'])))
 
-buffer <- 'THE FOLLOWING IS OLD DATA FOR CONTEXT, IT IS FOLLOWED BY A NEWLY DECLARED GOAL'
+#buffer <- 'THE FOLLOWING IS OLD DATA FOR CONTEXT, IT IS FOLLOWED BY A NEWLY DECLARED GOAL'
 
-newInformationContext <- c(buffer,InfoRow$Row,iterationRow[1,'newP'])
+#newInformationContext <- c(buffer,InfoRow$Row,iterationRow[1,'newP'])
 #########
 
-####Need to figure out how to continue looping and filling the iterationRow as well as updating the InfoRow
+processAdditionalRuns <- function(i){
+  
+  reset_messages()
+  
+  buffer <- 'THE FOLLOWING IS OLD DATA FOR CONTEXT, IT IS FOLLOWED BY A NEWLY DECLARED GOAL'
+  
+  newInformationContext <- c(buffer,InfoRow$Row,iterationRow[i,'newP'])
+  
+  userInputMessage = c(
+    newInformationContext
+  )
+  
+  addUserMessage(userMessage = userInputMessage)
+  pingAPI(userInputMessage = messages[[2]]$content,agentType = 'Semantic')
+  
+  
+  
+  previousChoices <- InfoRow %>% filter(Type == 'Choice Chosen')
+  choiceDisclaimer<- paste0('The choice cannot include ',c(previousChoices$Row))
+  goalMessage <- c(messages[[2]]$content,paste0('Semantic Keywords: ',messages[[3]]$content),
+                   choiceDisclaimer)
+  
+  
+  addUserMessage(userMessage = goalMessage)
+  pingAPI(userInputMessage = goalMessage,agentType = 'GoalGenerator')
+  
+  analyzeGoalMessage <- c(messages[[2]]$content,
+                          paste0('Semantic Keywords: ',messages[[3]]$content),
+                          paste0('Potential Choices: ',messages[[5]]$content)
+  )
+  addUserMessage(userMessage = analyzeGoalMessage)
+  pingAPI(userInputMessage = analyzeGoalMessage,agentType = 'GoalAnalyzer')
+  
+  choiceChooserMessage <- c(messages[[2]]$content,
+                            paste0('Semantic Keywords: ',messages[[3]]$content),
+                            paste0('Potential Choices: ',messages[[5]]$content),
+                            paste0('Analysis of Goals: ',messages[[7]]$content)
+  )
+  addUserMessage(userMessage = choiceChooserMessage)
+  pingAPI(userInputMessage = choiceChooserMessage,agentType = 'ChoiceChooser')
+  
+  
+  
+  disclaimerDifferenceVector <- paste0('Please only consider the impact of the most recent choice made which is ',
+                                      messages[[9]]$content)
+  
+  differenceVectorMessage <- c(messages[[2]]$content,
+                               paste0('Semantic Keywords: ',messages[[3]]$content),
+                               paste0('Potential Choices: ',messages[[5]]$content),
+                               paste0('Analysis of Goals: ',messages[[7]]$content),
+                               paste0('Choice Made: ',messages[[9]]$content),
+                               disclaimerDifferenceVector
+  )
+  
+  addUserMessage(userMessage = differenceVectorMessage)
+  pingAPI(userInputMessage = differenceVectorMessage,agentType = 'DifferenceVector')
+  
+  
+  semanticDifferenceVectorMessage <- c(messages[[2]]$content,
+                                       paste0('Potential Choices: ',messages[[5]]$content),
+                                       paste0('Choice Made: ',messages[[9]]$content),
+                                       paste0('Difference Vector after Choice Made: ',messages[[11]]$content)
+  )
+  
+  addUserMessage(userMessage = semanticDifferenceVectorMessage)
+  pingAPI(userInputMessage = semanticDifferenceVectorMessage,agentType = 'Semantic')
+  
+  
+ 
+  
+  disclaimerGoalReanalyze <- 'Remember that the choice has already been taken, so the goal
+  should adjust itself based on the choice made and the differences. You can adjust the goal
+  forwards'
+  
+  goalReAnalyzeMessage <- c(messages[[2]]$content,
+                            paste0('Semantic Keywords: ',messages[[3]]$content),
+                            paste0('Potential Choices: ',messages[[5]]$content),
+                            paste0('Analysis of Goals: ',messages[[7]]$content),
+                            paste0('Choice Made: ',messages[[9]]$content),
+                            paste0('Difference Vector after Choice Made: ',messages[[11]]$content),
+                            paste0('Semantic Difference Vector after Choice Made: ',messages[[13]]$content),
+                            disclaimerGoalReanalyze
+                            
+  )
+  
+  addUserMessage(userMessage = goalReAnalyzeMessage)
+  pingAPI(userInputMessage = goalReAnalyzeMessage,agentType = 'GoalReAnalyzer')
+  
+  # newFullInformationVector <- c(paste0('Original Goal and Information: ',messages[[2]]$content),
+  #                             paste0('Choice Made: ',messages[[9]]$content),
+  #                             paste0('Difference Vector after Choice Made: ',messages[[11]]$content),
+  #                             paste0('Semantic Difference Vector after Choice Made: ',messages[[13]]$content),
+  #                             paste0('New Goal Declared: ',messages[[15]]$content)
+  #   )
+  
+  #######
+  # InformationVector <- c(paste0('Original Goal and Information: ',messages[[2]]$content),
+  #                       paste0('Choice Made: ',messages[[9]]$content),
+  #                       paste0('Difference Vector after Choice Made: ',messages[[11]]$content),
+  #                       paste0('Semantic Difference Vector after Choice Made: ',messages[[13]]$content),
+  #                       paste0('New Goal Declared: ',messages[[15]]$content)
+  # )
+  #newGoal <- paste0('New Goal: ',messages[[15]]$content)
+  
+  #newState <- c(newGoal,'AND FOR CONTEXT: ', InformationVector)
+  #i <- 2
+  iterationRow[i,'TransformI'] <- messages[[3]]$content
+  iterationRow[i,'D'] <- messages[[5]]$content
+  iterationRow[i,'FM'] <- messages[[7]]$content
+  iterationRow[i,'Fn'] <- messages[[9]]$content
+  iterationRow[i,'Idelta'] <- messages[[11]]$content
+  iterationRow[i,'TIdelta'] <- messages[[13]]$content
+  iterationRow[i,'newP'] <- messages[[15]]$content
+  
+  #iterationRow <<- rbind(iterationRow,newRow)
+  
+  newRow <<- data.frame(n = i+1,
+                        pnull = iterationRow[i,'newP'], 
+                        I1 = paste0(iterationRow[i,'I1'],iterationRow[i,'Idelta']) , 
+                        TransformI = NA,
+                        D = NA,
+                        FM = NA,
+                        Fn = NA,
+                        Idelta = NA,
+                        TIdelta = NA,
+                        newP = NA)
+  
+  iterationRow <<- rbind(iterationRow,newRow)
+  
+  
+  InfoNewRow <- rbind(data.frame(Type= 'Goal For Run',Row = paste0('Goal For Run: ',iterationRow[i,'pnull'])),
+                   data.frame(Type= 'Information For Run',Row = paste0('Information For Run: ',iterationRow[i,'I1'])),
+                   data.frame(Type= 'Choices Considered',Row = paste0('Choices Considered: ',iterationRow[i,'D'])),
+                   data.frame(Type= 'Choice Chosen',Row = paste0('Choice Chosen: ',iterationRow[i,'Fn'])),
+                   data.frame(Type= 'New Information From Run',Row = paste0('New Information From Run: ',iterationRow[i,'Idelta'])))
+  
+  InfoRow <<- rbind(InfoRow,InfoNewRow)
+  
 
-#This begins the second loop content
-reset_messages()
-addUserMessage(userMessage = newInformationContext)
-pingAPI(userInputMessage = newInformationContext,agentType = 'Semantic')
+  
+}
+
+for(i in 2:5){
+  
+  processAdditionalRuns(i = i)
+  
+  
+}
+
 
 
